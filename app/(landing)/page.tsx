3a7594d8de4577f6c338504485075a3f10ea6d9f
Aquib <85285702+AquibPro@users.noreply.github.com>
2023-08-15 21:21:43 +0530

    Index

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -9,8 +9,8 @@ const LandingPage = () => {
     <div>
     <Head>
       <title>Aquib AI</title>
-      <meta name="title" content="Aquib AI" />
-      <meta name="description" content="Explore the frontier of AI! Chat with virtual intellects, craft images from thin air, get code assistance, and engage with famed AI personas—all in one dynamic hub. Dive into the future, today." />
+      <meta name="og:title" content="Aquib AI" />
+      <meta name="og:description" content="Explore the frontier of AI! Chat with virtual intellects, craft images from thin air, get code assistance, and engage with famed AI personas—all in one dynamic hub. Dive into the future, today." />
       <meta name="keywords" content="aquib, aquib khan, ai, ai image, einstein, leonardo da vinci, messi, lionel, william shakespeare, ai code" />
       <meta name="author" content="Aquib Khan" />
     </Head>
