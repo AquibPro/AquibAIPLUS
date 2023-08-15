cb83f334c500c8ac7d3a891882696fc571c866ee
Aquib <85285702+AquibPro@users.noreply.github.com>
2023-08-15 20:27:16 +0530

    &nbsp;

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -52,8 +52,8 @@ export const LandingContent = () => {
       </div>
       <footer className="bg-gray-800 text-white py-4 text-center w-full mt-24 mx-0 rounded-tl-xl rounded-tr-xl">
     <div className="flex justify-center items-center gap-x-10 text-white-400 py-4 px-0">
-      <a href="https://aquibai.aquibkhan.repl.co/privacy-policy.html" target="_blank">Privacy&nbsp;Policy</a>
-      <a href="https://aquibai.aquibkhan.repl.co/terms-of-service.html" target="_blank">Terms&nbsp;of&nbsp;Service</a>
+      <a href="https://aquibai.aquibkhan.repl.co/privacy-policy.html" target="_blank">Privacy Policy</a>
+      <a href="https://aquibai.aquibkhan.repl.co/terms-of-service.html" target="_blank">Terms of Service</a>
       <a href="https://aquibai.aquibkhan.repl.co/cancellation.html" target="_blank">Cancellation</a>
       <a href="mailto:support@aquibai.com">Email</a>
     </div>
