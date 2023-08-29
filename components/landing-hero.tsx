2b1a1a6f9051893d4020e5894f84987b824ed3cb
Aquib <85285702+AquibPro@users.noreply.github.com>
2023-08-29 17:02:13 +0530

    Update

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -29,7 +29,7 @@ export const LandingHero = () => {
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 100x faster.
+        AI of the people, for the people and by the people.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
