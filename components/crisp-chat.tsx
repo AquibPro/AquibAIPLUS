3c3134631091edf94ebef56c38c1c06beb90f4f1
Aquib <85285702+AquibPro@users.noreply.github.com>
2024-08-27 17:11:35 +0530

    Chat

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    
+    Crisp.configure("2cef74bb-c1b3-4949-bf3e-224020ed0727");
   }, []);
 
   return null;
--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -29,7 +29,7 @@ export const LandingHero = () => {
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        AI of the people and for the people.
+      Unleashing Power for Lightning-Fast Results!
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
